eda3a58bbeda262bccfaa68e422f84d50201a64c
Julian J. van der Giesen <julian@project89109.nl>
2016-12-12 07:22:58 +0100

    Add lodash

--- src/scripts/apiCalls.js
+++ src/scripts/apiCalls.js
@@ -21,10 +21,35 @@ $('[data-button="generate_graph"]').click(function () {
     axios.get('http://api.smashmail.nl/event/V1/getEventStatisticByEventProcessor?startDate='+ startDateTime.toISOString() +'&endDate='+ endDateTime.toISOString() +'&eventProcessor=' + eventProcessor + '')
         .then(function (response) {
             console.log(response.data.data);
-            response.data.data.forEach(function (mailObject) {
-                console.log(mailObject.created);
-                console.log(mailObject.data)
-            });
+
+            // console.log(_.filter(response.data.data, {
+            //   data: {
+            //     platform_id: 5,
+            //     action: "email_sent"
+            //   }
+            // }));
+
+
+            var summary = {
+              "data": [{
+                "created": "2016-12-05 00:00:00",
+                "data": {
+                  "action": "email_sent",
+                  "platform_id": 3
+                },
+                "total": "120889"
+              },]
+            }
+
+            console.log(_.filter(summary.data, {
+              data: {
+                platform_id: 3,
+                action: "email_sent"
+              }
+            }));
+
+
+
         })
         .catch(function (error) {
             console.log(error);
