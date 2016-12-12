a9246e0019ff65432f1358da1baf9a23a747301b
Julian van der Giesen <julian@project89109.nl>
2016-12-12 18:01:31 +0100

    Create labels moment.js

--- dist/scripts/custom.js
+++ dist/scripts/custom.js
@@ -15,46 +15,197 @@ axios.get('http://api.smashmail.nl/event/V1/getAllEventProcessors')
 $('[data-button="generate_graph"]').click(function () {
 
     var eventProcessor = $('[data-select="event_processors"]').val();
-    var startDateTime = $('#reportrange').data('daterangepicker').startDate._d;
-    var endDateTime = $('#reportrange').data('daterangepicker').endDate._d;
+    var startDateTime = $('#reportrange').data('daterangepicker').startDate;
+    var endDateTime = $('#reportrange').data('daterangepicker').endDate;
 
-    axios.get('http://api.smashmail.nl/event/V1/getEventStatisticByEventProcessor?startDate='+ startDateTime.toISOString() +'&endDate='+ endDateTime.toISOString() +'&eventProcessor=' + eventProcessor + '')
+    var labels = [];
+
+    getLabelInterval(startDateTime, endDateTime);
+
+    function getLabelInterval(startTimestamp, endTimestamp) {
+        var startMoment = moment(startTimestamp);
+        var endMoment = moment(endTimestamp);
+
+        if ((endMoment.diff(startMoment, 'months')) > 0) {
+
+        } else if ((endMoment.diff(startMoment, 'days')) > 0) {
+            createDayLabels(startMoment, endMoment);
+        } else if ((endMoment.diff(startMoment, 'hours')) > 0) {
+            createHourLabels(startMoment, endMoment);
+        } else if ((endMoment.diff(startMoment, 'minutes')) > 0) {
+
+        }
+        ;
+    }
+
+    function createDayLabels(startDate, endDate) {
+        var startMoment = new moment(startDate);
+        var endMoment = new moment(endDate);
+        var allLabels = [];
+        var startLabel = startMoment.floor(24, 'hours');
+        var endLabel = endMoment.floor(24, 'hours');
+
+        // loop till startlabel = endlabel + push into allLables
+        var currDate = startLabel.clone().subtract(1, 'days');
+        var lastDate = endLabel.clone().endOf('day');
+
+        while (currDate.add(1, 'days').diff(lastDate) < 0) {
+            allLabels.push(currDate.clone().format('DD-MM-YYYY'));
+        }
+        console.log(allLabels);
+    }
+
+    function createHourLabels(startDate, endDate) {
+        var startMoment = new moment(startDate);
+        var endMoment = new moment(endDate);
+        var allLabels = [];
+        var startLabel = startMoment.floor(1, 'hours');
+        var endLabel = endMoment.floor(1, 'hours');
+
+        console.log(startLabel);
+        console.log(endLabel);
+
+        // loop till startlabel = endlabel + push into allLables
+        var currDate = startLabel.clone().subtract(1, 'hours');
+        var lastDate = endLabel.clone().endOf('hour');
+
+        console.log( currDate.add(1, 'hours'));
+
+        while (currDate.add(1, 'hours').diff(lastDate) < 0) {
+            console.log(currDate.clone().format('H:mm'));
+            allLabels.push(currDate.clone().format('H:00'));
+        }
+        console.log(allLabels);
+    }
+
+
+    // createDayLabels(startDateTime, endDateTime);
+    //
+    // if ((moment(endDateTime).diff(moment(startDateTime), 'months')) > 0) {
+    //     console.log("Months");
+    // } else if ((moment(endDateTime).diff(moment(startDateTime), 'days')) > 0) {
+    //     console.log("days");
+    //
+    //     var currDate = moment(startDateTime).clone().subtract(1, 'days');
+    //     var lastDate = moment(endDateTime).clone().endOf('day');
+    //
+    //     while (currDate.add(1, 'days').diff(lastDate) < 0) {
+    //         labels.push(currDate.clone().format('DD-MM-YYYY'));
+    //     }
+    //     console.log(labels);
+    // } else if ((moment(endDateTime).diff(moment(startDateTime), 'hours')) > 0) {
+    //     console.log("hours");
+    //
+    //     var currDate = moment(startDateTime).clone().subtract(1, 'hours');
+    //     var lastDate = moment(endDateTime).clone().endOf('hour');
+    //
+    //     while (currDate.add(1, 'hours').diff(lastDate) < 0) {
+    //         labels.push(currDate.clone().format('H:HH'));
+    //     }
+    //     console.log(labels);
+    //
+    // } else if ((moment(endDateTime).diff(moment(startDateTime), 'minutes')) > 0) {
+    //     console.log("minuten");
+    // };
+
+
+    axios.get('http://api.smashmail.nl/event/V1/getEventStatisticByEventProcessor?startDate=' + startDateTime.toISOString() + '&endDate=' + endDateTime.toISOString() + '&eventProcessor=' + eventProcessor + '')
         .then(function (response) {
+            // convert string value to JS object
+            response.data.data.forEach(function (data) {
+                data.data = JSON.parse(data.data);
+            });
+
             console.log(response.data.data);
 
-            // console.log(_.filter(response.data.data, {
-            //   data: {
-            //     platform_id: 5,
-            //     action: "email_sent"
-            //   }
-            // }));
+
+            // Get all platforms ids
+            var platforms = _.map(response.data.data, function (platform) {
+                return platform.data.platform_id;
+            });
+
+            // Only keep unique platform id's
+            var uniquePlatforms = _.uniqBy(platforms, function (e) {
+                return e;
+            });
+
+            // Filter data per platform and create datasets
+
+            _.forEach(uniquePlatforms, function (platformId) {
+                var platformData = _.filter(response.data.data, {data: {platform_id: platformId}});
+            });
 
 
-            var summary = {
-              "data": [{
-                "created": "2016-12-05 00:00:00",
-                "data": {
-                  "action": "email_sent",
-                  "platform_id": 3
-                },
-                "total": "120889"
-              },]
-            }
+            var chartData = {
+                labels: labels,
+                datasets: [{
+                    label: "My First dataset",
+                    backgroundColor: "rgba(38, 185, 154, 0.31)",
+                    borderColor: "rgba(38, 185, 154, 0.7)",
+                    pointBorderColor: "rgba(38, 185, 154, 0.7)",
+                    pointBackgroundColor: "rgba(38, 185, 154, 0.7)",
+                    pointHoverBackgroundColor: "#fff",
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointBorderWidth: 1,
+                    data: [null, 20, 30, 50],
+                }, {
+                    label: "My Second dataset",
+                    backgroundColor: "rgba(38, 185, 154, 0.31)",
+                    borderColor: "rgba(38, 185, 154, 0.7)",
+                    pointBorderColor: "rgba(38, 185, 154, 0.7)",
+                    pointBackgroundColor: "rgba(38, 185, 154, 0.7)",
+                    pointHoverBackgroundColor: "#fff",
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointBorderWidth: 1,
+                    data: [30, 50, 30, 50],
+                }]
+            };
 
-            console.log(_.filter(summary.data, {
-              data: {
-                platform_id: 3,
-                action: "email_sent"
-              }
-            }));
-
-
+            drawGraph('chartCanvas', 'line', chartData);
 
         })
         .catch(function (error) {
             console.log(error);
         });
 });
+
+
+function getEventDatabyTimeframe(eventProcessor, startDate, endDate) {
+    axios.get('http://api.smashmail.nl/event/V1/getEventStatisticByEventProcessor?startDate=' + startDate.toISOString() + '&endDate=' + endDate.toISOString() + '&eventProcessor=' + eventProcessor + '')
+        .then(function (response) {
+
+            response.data.data.forEach(function (data) {
+                data.data = JSON.parse(data.data);
+            });
+
+            return response.data.data;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function filterEventData(eventData, filters) {
+
+}
+
+/**
+ * Drawing a chart on the canvas based on the following params:
+ * @param chartContextId
+ * @param chartType
+ * @param chartData
+ */
+
+function drawGraph(chartContextId, chartType, chartData) {
+
+    var ctx = document.getElementById(chartContextId);
+
+    var chart = new Chart(ctx, {
+        type: chartType,
+        data: chartData,
+    });
+
+}
 
 /**
  * To change this license header, choose License Headers in Project Properties.
