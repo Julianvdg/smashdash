a9176022e7ab4f917583b3ee7b9fa125869ec360
Julian van der Giesen <julian@project89109.nl>
2016-12-13 17:03:37 +0100

    Add data to chart

--- src/scripts/apiCalls.js
+++ src/scripts/apiCalls.js
@@ -14,13 +14,12 @@ axios.get('http://api.smashmail.nl/event/V1/getAllEventProcessors')
 // Create chart based on filled in params(startDate, endDate, eventProcessor) | request made to http://api.smashmail.nl/event/V1/getEventStatisticByEventProcessor
 $('[data-button="generate_graph"]').click(function () {
 
-    var eventProcessor = $('[data-select="event_processors"]').val();
-    var startDateTime = $('#reportrange').data('daterangepicker').startDate;
-    var endDateTime = $('#reportrange').data('daterangepicker').endDate;
-
-    var labels = [];
-
-    getLabelInterval(startDateTime, endDateTime);
+    var eventProcessor = $('[data-select="event_processors"]').val(),
+        startDateTime = $('#reportrange').data('daterangepicker').startDate,
+        endDateTime = $('#reportrange').data('daterangepicker').endDate,
+        chartType = $('input[name=chartTypeRadio]:checked').val(),
+        formatUsedForLabels = '',
+        labels = [];
 
     function getLabelInterval(startTimestamp, endTimestamp) {
         var startMoment = moment(startTimestamp);
@@ -34,75 +33,73 @@ $('[data-button="generate_graph"]').click(function () {
             return createLabels("Hour", startMoment, endMoment);
         } else if ((endMoment.diff(startMoment, 'minutes')) > 0) {
             return createLabels("Minute", startMoment, endMoment);
-        };
+        }
+        ;
     }
 
     function createLabels(timeframe, startDate, endDate) {
-      var startMoment = new moment(startDate);
-      var endMoment = new moment(endDate);
-      var allLabels = [];
+        var startMoment = new moment(startDate);
+        var endMoment = new moment(endDate);
+        var allLabels = [];
 
-      switch(timeframe) {
-        case "Month":
-            var startLabel = startMoment;
-            var endLabel = endMoment;
+        switch (timeframe) {
+            case "Month":
+                formatUsedForLabels = 'MM-YYYY';
+                var startLabel = startMoment;
+                var endLabel = endMoment;
 
-            var currDate = startLabel.clone().subtract(1, 'months');
-            var lastDate = endLabel.clone().endOf('month');
+                var currDate = startLabel.clone().subtract(1, 'months');
+                var lastDate = endLabel.clone().endOf('month');
 
-            while (currDate.add(1, 'months').diff(lastDate) < 0) {
-                allLabels.push(currDate.clone().format('MM-YYYY'));
-            }
+                while (currDate.add(1, 'months').diff(lastDate) < 0) {
+                    allLabels.push(currDate.clone().format('MM-YYYY'));
+                }
 
-            break;
-      case "Day":
-          var startLabel = startMoment.floor(24, 'hours');
-          var endLabel = endMoment.floor(24, 'hours');
+                break;
+            case "Day":
+                formatUsedForLabels = 'DD-MM-YYYY';
+                var startLabel = startMoment.floor(24, 'hours');
+                var endLabel = endMoment.floor(24, 'hours');
 
-          // loop till startlabel = endlabel + push into allLables
-          var currDate = startLabel.clone().subtract(1, 'days');
-          var lastDate = endLabel.clone().endOf('day');
+                var currDate = startLabel.clone().subtract(1, 'days');
+                var lastDate = endLabel.clone().endOf('day');
 
-          while (currDate.add(1, 'days').diff(lastDate) < 0) {
-              allLabels.push(currDate.clone().format('DD-MM-YYYY'));
-          }
+                while (currDate.add(1, 'days').diff(lastDate) < 0) {
+                    allLabels.push(currDate.clone().format('DD-MM-YYYY'));
+                }
 
-          break;
-      case "Hour":
-          var startLabel = startMoment.floor(1, 'hours');
-          var endLabel = endMoment.floor(1, 'hours');
+                break;
+            case "Hour":
+                formatUsedForLabels = 'H:mm';
+                var startLabel = startMoment.floor(1, 'hours');
+                var endLabel = endMoment.floor(1, 'hours');
 
-          // loop till startlabel = endlabel + push into allLables
-          var currDate = startLabel.clone().subtract(1, 'hours');
-          var lastDate = endLabel.clone().endOf('hour');
+                var currDate = startLabel.clone().subtract(1, 'hours');
+                var lastDate = endLabel.clone().endOf('hour');
 
+                while (currDate.add(1, 'hours').diff(lastDate) < 0) {
+                    allLabels.push(currDate.clone().format('H:mm'));
+                }
 
-          while (currDate.add(1, 'hours').diff(lastDate) < 0) {
-              allLabels.push(currDate.clone().format('H:mm'));
-          }
+                break;
+            case "Minute":
+                formatUsedForLabels = 'H:mm';
+                var startLabel = startMoment.floor(1, 'minutes');
+                var endLabel = endMoment.floor(1, 'minutes');
 
-          break;
-      case "Minute":
-          var startLabel = startMoment.floor(1, 'minutes');
-          var endLabel = endMoment.floor(1, 'minutes');
+                var currDate = startLabel.clone().subtract(1, 'minutes');
+                var lastDate = endLabel.clone().endOf('minute');
 
-          // loop till startlabel = endlabel + push into allLables
-          var currDate = startLabel.clone().subtract(1, 'minutes');
-          var lastDate = endLabel.clone().endOf('minute');
+                while (currDate.add(1, 'minutes').diff(lastDate) < 0) {
+                    allLabels.push(currDate.clone().format('H:mm'));
+                }
 
+                break;
 
-          while (currDate.add(1, 'minutes').diff(lastDate) < 0) {
-              allLabels.push(currDate.clone().format('H:mm'));
-          }
-
-          break;
-
-      }
-      return allLabels;
+        }
+        return allLabels;
 
     }
-
-
 
 
     axios.get('http://api.smashmail.nl/event/V1/getEventStatisticByEventProcessor?startDate=' + startDateTime.toISOString() + '&endDate=' + endDateTime.toISOString() + '&eventProcessor=' + eventProcessor + '')
@@ -112,8 +109,11 @@ $('[data-button="generate_graph"]').click(function () {
                 data.data = JSON.parse(data.data);
             });
 
-            console.log(response.data.data);
+            $("#chartTitle").html('<h2>' + eventProcessor + ' |<span> ' + moment(startDateTime).format('DD-MM-YYYY, H:mm') + ' / ' + moment(endDateTime).format('DD-MM-YYYY, H:mm') + '</span>' + '</h2>');
 
+            var dataSets = [];
+
+            var allLabels = getLabelInterval(startDateTime, endDateTime)
 
             // Get all platforms ids
             var platforms = _.map(response.data.data, function (platform) {
@@ -125,42 +125,50 @@ $('[data-button="generate_graph"]').click(function () {
                 return e;
             });
 
-            // Filter data per platform and create datasets
+            getPlatformNamesByPlatformIds(uniquePlatforms);
 
-            _.forEach(uniquePlatforms, function (platformId) {
-                var platformData = _.filter(response.data.data, {data: {platform_id: platformId}});
-            });
+            for (var pi = 0; pi < uniquePlatforms.length; pi++) {
+                var color = randomChartColor();
+                var platformId = uniquePlatforms[pi];
+                dataSets[pi] = {
+                    label: "Platform " + platformId,
+                    backgroundColor: color + ",0.5" + ")",
+                    borderColor: color + ", 1" + ")",
+                    pointBorderColor: color + ",0.7" + ")",
+                    pointBackgroundColor: color + ", 0.7" + ")",
+                    pointHoverBackgroundColor: "#fff",
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointBorderWidth: 1,
+                    data: [],
+                };
+                for (var li = 0; li < allLabels.length; li++) {
+                    var currentLabel = allLabels[li],
+                        foundValue   = false;
+                    for (var i = 0; i < response.data.data.length; i++) {
+                        var responseData = response.data.data[i],
+                            createdMoment = new moment(responseData.created),
+                            dateFormattedForLabel = createdMoment.format(formatUsedForLabels);
+                        if (
+                            responseData.data.platform_id === platformId &&
+                            currentLabel === dateFormattedForLabel
+                        ) {
+                            dataSets[pi].data.push(responseData.total);
+                            foundValue = true;
+                            break;
+                        }
+                    }
+                    if (false === foundValue) {
+                        dataSets[pi].data.push(null);
+                    }
+                }
+            }
 
-
-
-
-            console.log(getLabelInterval(startDateTime, endDateTime));
             var chartData = {
                 labels: getLabelInterval(startDateTime, endDateTime),
-                datasets: [{
-                    label: "My First dataset",
-                    backgroundColor: "rgba(38, 185, 154, 0.31)",
-                    borderColor: "rgba(38, 185, 154, 0.7)",
-                    pointBorderColor: "rgba(38, 185, 154, 0.7)",
-                    pointBackgroundColor: "rgba(38, 185, 154, 0.7)",
-                    pointHoverBackgroundColor: "#fff",
-                    pointHoverBorderColor: "rgba(220,220,220,1)",
-                    pointBorderWidth: 1,
-                    data: [null, 20, 30, 50],
-                }, {
-                    label: "My Second dataset",
-                    backgroundColor: "rgba(38, 185, 154, 0.31)",
-                    borderColor: "rgba(38, 185, 154, 0.7)",
-                    pointBorderColor: "rgba(38, 185, 154, 0.7)",
-                    pointBackgroundColor: "rgba(38, 185, 154, 0.7)",
-                    pointHoverBackgroundColor: "#fff",
-                    pointHoverBorderColor: "rgba(220,220,220,1)",
-                    pointBorderWidth: 1,
-                    data: [30, 50, 30, 50],
-                }]
+                datasets: dataSets,
             };
 
-            drawGraph('chartCanvas', 'line', chartData);
+            drawGraph('chartCanvas', chartType, chartData);
 
         })
         .catch(function (error) {
@@ -168,25 +176,34 @@ $('[data-button="generate_graph"]').click(function () {
         });
 });
 
+function randomChartColor() {
+    var r = Math.floor(Math.random() * 255);
+    var g = Math.floor(Math.random() * 255);
+    var b = Math.floor(Math.random() * 255);
+    return "rgba(" + r + "," + g + "," + b ;
+};
 
-function getEventDatabyTimeframe(eventProcessor, startDate, endDate) {
-    axios.get('http://api.smashmail.nl/event/V1/getEventStatisticByEventProcessor?startDate=' + startDate.toISOString() + '&endDate=' + endDate.toISOString() + '&eventProcessor=' + eventProcessor + '')
-        .then(function (response) {
 
-            response.data.data.forEach(function (data) {
-                data.data = JSON.parse(data.data);
-            });
+function getPlatformNamesByPlatformIds(platformIds) {
 
-            return response.data.data;
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-}
+    $.ajax({
+        url : 'http://api.smashmail.nl/platform/V1/getPlatformsByPlatformIds',
+        data : {platformIds:platformIds},
+        type: 'POST',
+        error : function (data, textStatus, jqXHR) {
+            if(data.statusText != "abort")
+                console.log(data);
+        },
+        success : function (data) {
+            console.log(data);
+        }
+    });
 
-function filterEventData(eventData, filters) {
+};
 
-}
+
+
+
 
 /**
  * Drawing a chart on the canvas based on the following params:
@@ -195,13 +212,19 @@ function filterEventData(eventData, filters) {
  * @param chartData
  */
 
-function drawGraph(chartContextId, chartType, chartData) {
+var chart = null;
 
+function drawGraph(chartContextId, chartType, chartData) {
+    if(chart!=null){
+        chart.destroy();
+    }
     var ctx = document.getElementById(chartContextId);
 
-    var chart = new Chart(ctx, {
+    chart = new Chart(ctx, {
         type: chartType,
         data: chartData,
     });
+
+    console.log(chart);
 
 }
